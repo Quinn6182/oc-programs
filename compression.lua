242436462191995add51ba780b6898721ed142b1
Quinn6182 <quinnhayden2010@gmail.com>
2023-02-15 13:58:53 -0800

    Updated Things

--- compression.lua
+++ compression.lua
@@ -1,6 +1,9 @@
 local data = require("component").data
-local function list_dir()
-    for i in filesystem.list("/") do
-        print(i)
-    end
+local filesystem = require("filesystem")
+print("What is the directory? ")
+local dir = io.read()
+print("Inflate or Deflate (i or d)? ")
+local action = io.read()
+for i in filesystem.list(dir) do
+    print(i)
 end
